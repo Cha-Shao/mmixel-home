9dea64d781a78efe27e4ed437b309ca7e2f66f1c
Cha_Shao <yrchashao@outlook.com>
2023-06-15 20:38:03 +0800

    头像选中显示

--- components/pages/page/MembersM/MembersView.tsx
+++ components/pages/page/MembersM/MembersView.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { Member } from "../Members"
 import Image from "next/image"
 import MemberCard from "../Members/MemberCard"
+import classNames from "classnames"
 
 const MembersView = (props: Member[]) => {
   const [index, setIndex] = useState<number>(0)
@@ -14,7 +15,11 @@ const MembersView = (props: Member[]) => {
         {members.map((member, i) =>
           <Image key={i} src={member.avatar} alt="Avatar"
             width={48} height={48}
-            className="snap-start scroll-mx-2 mr-2 shrink-0 rounded-full"
+            className={classNames(
+              'snap-start scroll-mx-2 mr-2 shrink-0 rounded-full',
+              'border-2 duration-200',
+              i === index ? 'border-main' : 'border-transparent'
+            )}
             onClick={() => { setIndex(i) }} />
         )}
         <div className="snap-start scroll-mx-2 mr-4 shrink-0" />
